88e2b6052b2ef8742f1d2bfc1b46f0b99e9e104a
marctraber <traber.marc@hispeed.ch>
2021-02-10 22:42:48 +0100

    Add Equals

--- PetManager/PetManager/Pet.cs
+++ PetManager/PetManager/Pet.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetManager {
-    public class Pet {
+namespace PetManager
+{
+    public class Pet
+    {
         public string Name { get; set; }
         public string Breed { get; set; }
         public string Birthday { get; set; }
@@ -21,8 +23,22 @@ namespace PetManager {
             unchecked // Overflow is fine, just wrap 
             {
                 int hash = 17;
-                hash = hash * 23 + (this.Name != null ? this.Name.GetHashCode() : 0); hash = hash * 23 + (this.Breed != null ? this.Breed.GetHashCode() : 0); return hash;
+                hash = hash * 23 + (this.Name != null ? this.Name.GetHashCode() : 0);
+                hash = hash * 23 + (this.Breed != null ? this.Breed.GetHashCode() : 0);
+                return hash;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Pet))
+            {
+                return object.Equals(obj, this);
+            }
+
+            var pet = (Pet) obj;
+            return string.Equals(this.Name, pet.Name) && Birthday.Equals(pet.Birthday) &&
+                   string.Equals(this.Breed, pet.Breed);
         }
     }
 }
