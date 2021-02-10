f548a87fe7f6a0f3d55e7de32f78fca385cefa91
marctraber <traber.marc@hispeed.ch>
2021-02-10 22:31:14 +0100

    Add Age and bDay

--- PetManager/PetManager/Pet.cs
+++ PetManager/PetManager/Pet.cs
@@ -8,5 +8,12 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+        public string Birthday { get; set; }
+        public int Age { get; set; }
+
+        public Pet(string bDay)
+        {
+            Birthday = bDay;
+        }
     }
 }
